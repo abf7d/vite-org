02e531472576367930f673ca99d520db83e9c8d5
Aaron Friedman <afriedman111@gmail.com>
2024-08-26 09:18:08 -0400

    fix: fixed env variables for vite

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,24 @@
 import { defineConfig } from 'vite';
+import { createHtmlPlugin } from 'vite-plugin-html';
+import { loadEnv } from 'vite'
 
-export default defineConfig({
+
+export default ({ mode }) => {
+const env = loadEnv(mode, process.cwd(), '')
+return defineConfig({
   build: {
     outDir: 'docs', // or 'dist'
   },
   base: './', // Base path for GitHub Pages
+  plugins: [
+    createHtmlPlugin({
+      inject: {
+        data: {
+          baseUrl: env.APP_BASE_URL,
+        },
+      },
+    }),
+  ],
 });
+
+};
